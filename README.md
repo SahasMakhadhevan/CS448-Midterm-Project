27818ab198cddf8e4c8f631cfb3dca87585dd72b
SahasMakhadhevan <code@sahas.dev>
2023-10-07 17:23:18 -0400

    Added comments, unified the code. - Comments and documentation added - Unified the code between BC and SVM - Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CS448-Midterm-Project
+
+## Project Description
+We were given a dataset of sentences and the corresponding POS tags for each word. We were tasked with writing 3 algorithms to tag the test data appropriately. 
+The algorithms we wrote were:
+- Bayesian Classifier
+- Logistic Regression
+- Support Vector Machine
+
+## Collaborators
+- Anushka Verma
+- Hannah Sugden
+- Sam Naemi
+- Sahas Makhadhevan
+- Dhanvi Meda
--- bayesian_classifier.py
+++ bayesian_classifier.py
@@ -1,6 +1,4 @@
-# Write the code for bayesian classifier algo here
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
@@ -14,15 +12,17 @@ output_filename = 'data/bc_output.txt'
 def get_features(index, sentence):
     return {
         'word': sentence[index],
-        'is_first': index == 0,
-        'is_last': index == len(sentence) - 1,
-        'capital': sentence[index] == (sentence[index].title()),
         'prev_word': '' if index == 0 else sentence[index - 1],
         'next_word': '' if index == len(sentence) - 1 else sentence[index + 1],
+        'bool_first_letter_capital': sentence[index] == (sentence[index].title()),
+        'bool_first_word': index == 0,
+        'bool_last': index == len(sentence) - 1,
+        'bool_numeric': sentence[index].isdigit()
     }
 
 
 def train_model():
+    # Load training data
     train_data = []
     with open(training_data, 'r') as file:
         sentence_tokens = []
@@ -34,20 +34,25 @@ def train_model():
             else:
                 train_data.append(sentence_tokens)
                 sentence_tokens = []
+
+    # Extract features
     x_train = [[get_features(index, sentence) for index in range(len(sentence))] for sentence in
                [list(zip(*sentence))[0] for sentence in train_data]]
     x_train = [word for sentence in x_train for word in sentence]
     y_train = [word[1] for sentence in train_data for word in sentence]
 
+    # Train model
     model = Pipeline([
         ('vectorizer', DictVectorizer(sparse=True)),
         # ('scaler', StandardScaler(with_mean=False)),
         ('classifier', MultinomialNB())
     ])
 
+    # https://youtu.be/l2I8NycJMCY?si=_Wp4T-JWuZhxWtQp
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2)
     model.fit(x_train, y_train)
 
+    # Evaluate model
     accuracy = model.score(x_test, y_test)
     print("Accuracy of dev set using BC: ", accuracy)
     return model
@@ -58,6 +63,7 @@ def predict_tag():
     # Load the selected model
     model = train_model()
 
+    # Load test data
     test_data = []
     with open(test_filename, 'r') as file:
         tokens = []
@@ -69,17 +75,17 @@ def predict_tag():
                 test_data.append(tokens)
                 tokens = []
 
+    # Extract features
     x_test = [get_features(index, sentence) for sentence in test_data for index in range(len(sentence) - 1)]
-
     y_pred = [model.predict(sentence_features) for sentence_features in x_test]
-
     test_data = [word for sentence in test_data for word in sentence]
 
+    # Write output to file
     with open(output_filename, 'w') as f:
         for token, tag in zip(test_data, y_pred):
             f.write(f"{token} {tag[0]}\n")
         f.write("\n")
-    print("Done")
+    print("Output written to ", output_filename)
 
 
 predict_tag()
